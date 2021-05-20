6cae393a4256a3dd80e8b2d7d23c7a01b69bb459
Shubi <shubham683@gmail.com>
2021-05-20 09:48:06 +0530

    Added line comments to the main file

--- Playlist.py
+++ Playlist.py
@@ -2,7 +2,10 @@ import re
 from datetime import timedelta
 from googleapiclient.discovery import build
 
+# Put your API Key inplace of "MY_API_KEY" below
 api_key = "MY_API_KEY"
+
+# creating youtube resource object
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 hours_pattern = re.compile(r'(\d+)H')
@@ -11,21 +14,27 @@ seconds_pattern = re.compile(r'(\d+)S')
 
 total_seconds = 0
 
+# To handle pagination
 nextPageToken = None
 while True:
+    # First api request
+    # To fetch the video IDs of all the videos in a playlist
     pl_request = youtube.playlistItems().list(
         part='contentDetails',
         playlistId="PLC1og_v3eb4juFD_2YcJt2nDhytHNzhRU",
-        maxResults=10,
+        maxResults=50,
         pageToken=nextPageToken
     )
 
     pl_response = pl_request.execute()
 
+    # Saving all the videoIds in vid_ids array
     vid_ids = []
     for item in pl_response['items']:
         vid_ids.append(item['contentDetails']['videoId'])
 
+    # Second api request
+    # To fetch the duration of all the videos corresponding to their videoIds
     vid_request = youtube.videos().list(
         part="contentDetails",
         id=','.join(vid_ids)
@@ -57,9 +66,11 @@ while True:
     if not nextPageToken:
         break
 
+# Dividing the total seconds in seconds, minutes and hours
 total_seconds = int(total_seconds)
 
 minutes, seconds = divmod(total_seconds, 60)
 hours, minutes = divmod(minutes, 60)
 
+# Printing the total duration of the playlist in HH:MM:SS format
 print(f'{hours}:{minutes}:{seconds}')
